5dd9f9d4b624c221cdee9af7d8313556dadf9b72
gabrielacorona <gabriela.corona03@gmail.com>
2020-04-08 22:12:18 -0500

    finished all demo pages, maps and button functionality missing

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var slideIndex = 0;
+const API_KEY = "AIzaSyDVV83q2YmvcJBm2WUSWb_Kq17K0tpXtMs";
 
+var url = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`
 
 function showSlides() {
     var i;
@@ -15,8 +17,32 @@ function showSlides() {
     setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 
+
+
+
+function initMap() {
+   
+    var tec = {
+        lat: 25.6519834,
+        lng: -100.291414
+    };
+    // The map, centered at tec
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: tec
+        });
+    // The marker, positioned at tec
+    var marker = new google.maps.Marker({
+        position: tec,
+        map: map
+    });
+}
+
+
 function init() {
     showSlides();
+    initMap();
 }
 
 init();
