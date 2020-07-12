39d3acc8845cc385788767022edadd23d4509a45
gabrielacorona <gabriela.corona03@gmail.com>
2020-07-12 14:53:05 -0500

    aws implementation

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/microfluidicsdb"
 exports.JWT_KEY = process.env.JWT_KEY || 'secret'
-exports.PORT = process.env.PORT || '8080';
+exports.PORT = process.env.PORT || '8000';
