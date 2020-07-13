6a786b85a3394c90e1247554655b7dadd7fa85b8
gabrielacorona <gabriela.corona03@gmail.com>
2020-07-13 00:07:15 -0500

    heroku config

---
+++ s3.env.js
@@ -0,0 +1,8 @@
+const env = {
+	AWS_ACCESS_KEY: 'AKIA54QCHKBSHIKJVR4X',
+	AWS_SECRET_ACCESS_KEY: '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',
+	REGION: 'us-east-2',
+	Bucket: 'cd-microfluidics'
+};
+
+module.exports = env;
