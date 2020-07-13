edbc7cf2f3991fcb198339f481bf759221f240cb
gabrielacorona <gabriela.corona03@gmail.com>
2020-07-13 00:41:51 -0500

    env variables change no jala

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/microfluidicsdb"
 exports.JWT_KEY = process.env.JWT_KEY || 'secret'
 exports.PORT = process.env.PORT || '8000';
+no jala
