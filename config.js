0df79ee65c4db68961438731421e7ca04c809b32
gabrielacorona <gabriela.corona03@gmail.com>
2020-07-13 20:13:20 -0500

    heroku config

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.DATABASE_URL = process.env.DATABASE_URL
+exports.JWT_KEY = process.env.JWT_KEY
+exports.PORT = process.env.PORT
+exports.AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY
+exports.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+exports.REGION = process.env.REGION
+exports.Bucket = process.env.Bucket
